08b3af823ee7c7c251514a0b3bf8508330c25c3e
Gideon <gideonphiri032@gmail.com>
2024-09-03 02:36:13 +0000

    task 3: pycodestyle fix

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
-from typing import Dict
+from typing import Dict, List
 
 
 class Server:
-    # Previous implementation remains the same...
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset."""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]  # Exclude header
+        return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
         """Dataset indexed by position."""
         if self.__indexed_dataset is None:
             dataset = self.dataset()
-            self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
@@ -21,8 +37,9 @@ class Server:
         indexed_data = self.indexed_dataset()
 
         for i in range(page_size):
-            if next_index in indexed_data:
-                data.append(indexed_data[next_index])
+            while next_index not in indexed_data:
+                next_index += 1
+            data.append(indexed_data[next_index])
             next_index += 1
 
         return {
@@ -31,4 +48,3 @@ class Server:
             'page_size': len(data),
             'data': data,
         }
-
